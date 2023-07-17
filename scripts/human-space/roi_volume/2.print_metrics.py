26c75a84275f6304b5d1f117d3a61c8d1d6812ef
gagdiez <gagdiez.c@gmail.com>
2023-07-17 11:41:03 +0200

    small fix on print
    The `mean` volume was being computed wrongly as the `lh` and `rh` entries were being accumulated across chimps.

--- scripts/human-space/roi_volume/2.print_metrics.py
+++ scripts/human-space/roi_volume/2.print_metrics.py
@@ -9,6 +9,7 @@ volumes = {'lh': [], 'rh': []}
 for name, regions in zip(["BA44", "BA45"], [[2,4], [3,5]]):
     print(name)
     for r, h in zip(regions, ['lh', 'rh']):
+        volumes[h] = []
         for c in chimps:
             data = citrix.load(f'./results/{c}.{h}.{name}.nii').get_fdata() == r
             volumes[h].append(data.sum())
